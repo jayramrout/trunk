d6372c4178a014e73fc3ac1cea71bdf240eb4d39
Jayram <jayram.rout@gmail.com>
2015-03-28 00:22:11 -0400

    Updates

--- CoreJava/src/jrout/tutorial/corejava/io/ByteStreamExample.java
+++ CoreJava/src/jrout/tutorial/corejava/io/ByteStreamExample.java
@@ -1,9 +1,10 @@
 package jrout.tutorial.corejava.io;
 
 import java.io.BufferedInputStream;
-import java.io.File;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Java byte streams are used to perform input and output of 8-bit bytes. Though
@@ -16,43 +17,76 @@ import java.io.FileOutputStream;
  *
  */
 public class ByteStreamExample {
+	private static String inputFile = "C:\\Users\\Jayram\\git\\trunk\\CoreJava\\src\\jrout\\tutorial\\corejava\\io\\Person.java";
+	private static String outputFile = "C:\\Users\\Jayram\\git\\trunk\\CoreJava\\src\\jrout\\tutorial\\corejava\\io\\Person.java";
+	
+	public static void main(String args[]) throws IOException {
+		byteStreamDirect();
+		byteStreamUsingBuffer();
+	}
 
-	public static void main(String args[]) throws Exception {
-
-		/*
-		 * File inputFile = new File(
-		 * "C:/Users/Jayram/git/April-2014-Batch/CoreJava/src/com/h2kinfosys/java/io/Person.javaj"
-		 * ); System.out.println(inputFile.exists());
-		 */
-
-		FileInputStream in = null;
-		FileOutputStream out = null;
-		BufferedInputStream binStream = null;
+	private static void byteStreamDirect() throws IOException {
+		FileInputStream fileInputStream = null;
+		FileOutputStream fileOutputStream = null;
 		try {
-			File inputFile = new File(
-					"C:\\Users\\Jayram\\git\\trunk\\CoreJava\\src\\jrout\\tutorial\\corejava\\io\\Person.java");
-			
-			in = new FileInputStream(inputFile);
-			binStream = new BufferedInputStream(in);
-			
-			out = new FileOutputStream(
-					"C:/Users/Jayram/git/trunk/CoreJava/src/jrout/tutorial/corejava/io/PersonTemp2.txt");
-
+			fileInputStream = new FileInputStream(
+					inputFile);
+			fileOutputStream = new FileOutputStream(
+					outputFile);
 			int c;
-			while ((c = in.read()) != -1) {
-				out.write(c);
+			while ((c = fileInputStream.read()) != -1) {
+				/*
+				 * If you want to print the output in the console then use (char)c) i.e. casting the int back to char 
+				 */
+//				System.out.print(((char)c));
+				fileOutputStream.write(c);
 			}
-			// do some logic here..and it breaks...
-			
-			System.out.println("Written Successfully!!!");
+			System.out.println("Written Succesfully");
 		} finally {
-			if (in != null) {
-				in.close();
+			if (fileInputStream != null) {
+				fileInputStream.close();
 			}
-			if (out != null) {
-				out.close();
+			if (fileOutputStream != null) {
+				fileOutputStream.close();
 			}
 		}
 	}
-
+	
+	/**
+	 * Using BufferedInputStream
+	 * @throws IOException
+	 */
+	private static void byteStreamUsingBuffer() throws IOException {
+		FileInputStream fileInputStream = null;
+		FileOutputStream fileOutputStream = null;
+		BufferedInputStream bufferedInputStream = null;
+		BufferedOutputStream bufferedOutputStream = null;
+		int c;
+		try {
+			fileInputStream = new FileInputStream(inputFile);
+			bufferedInputStream = new BufferedInputStream(fileInputStream);
+			
+			fileOutputStream = new FileOutputStream(outputFile);
+			bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+			
+			while ((c = bufferedInputStream.read()) != -1) {
+				bufferedOutputStream.write(c);
+			}
+			System.out.println("Written Succesfully");
+		} finally {
+			if(bufferedInputStream != null ) {
+				bufferedInputStream.close();
+			}
+			if(bufferedOutputStream != null ) {
+				bufferedOutputStream.close();
+			}
+			
+			if (fileInputStream != null) {
+				fileInputStream.close();
+			}
+			if (fileOutputStream != null) {
+				fileOutputStream.close();
+			}
+		}
+	}
 }
