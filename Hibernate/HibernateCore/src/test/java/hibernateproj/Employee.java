7be7b7b861278bac1382a0c8c60f8b0c9846c004
Jayram Rout <jayram.rout@gmail.com>
2015-04-04 23:43:48 -0400

    Updates

---
+++ Hibernate/HibernateCore/src/test/java/hibernateproj/Employee.java
@@ -0,0 +1,35 @@
+package hibernateproj;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable
+{
+	private int emp_id;
+	private String emp_name;
+	private String emp_dept;
+	
+	public Employee () 
+	{
+		// Constructor class
+	}
+	
+	public int getEmp_id() {
+		return emp_id;
+	}
+	public void setEmp_id(int emp_id) {
+		this.emp_id = emp_id;
+	}
+
+	public String getEmp_name() {
+		return emp_name;
+	}
+	public void setEmp_name(String emp_name) {
+		this.emp_name = emp_name;
+	}
+	public String getEmp_dept() {
+		return emp_dept;
+	}
+	public void setEmp_dept(String emp_dept) {
+		this.emp_dept = emp_dept;
+	}
+}
