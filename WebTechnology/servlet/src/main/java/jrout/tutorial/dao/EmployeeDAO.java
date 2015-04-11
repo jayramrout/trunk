e9be257d77bfa825bfdcf48f589a0427e556b7c7
Jayram <jayram.rout@gmail.com>
2015-04-10 22:27:38 -0400

    Update

---
+++ WebTechnology/servlet/src/main/java/jrout/tutorial/dao/EmployeeDAO.java
@@ -0,0 +1,7 @@
+package jrout.tutorial.dao;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+	public List getEmployeeDetails(int salary);
+}
---
+++ WebTechnology/servlet/src/main/java/jrout/tutorial/dao/EmployeeDAOImpl.java
@@ -0,0 +1,10 @@
+package jrout.tutorial.dao;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+	
+	public List getEmployeeDetails(int salary){
+		return null;
+	}
+}
