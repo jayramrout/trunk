ff56d2ace5a27a211964a3829caeb843ebb33bbd
Jayram <jayram.rout@gmail.com>
2015-07-29 20:30:59 -0400

    update

---
+++ CoreJava/src/main/java/jrout/tutorial/corejava/jdbc/domains/Employee.java
@@ -0,0 +1,26 @@
+package jrout.tutorial.corejava.jdbc.domains;
+
+public class Employee {
+	private String firstName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public double getBonus() {
+		return bonus;
+	}
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	private double salary;
+	private double bonus;
+	
+}
