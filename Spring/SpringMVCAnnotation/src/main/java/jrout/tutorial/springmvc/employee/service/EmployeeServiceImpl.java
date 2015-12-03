4ad2fc9650298fb5296b32cfed3b0bd9c2189569
Jayram <jayram.rout@hotmail.com>
2015-12-02 20:43:46 -0500

    Update

--- Spring/SpringMVCAnnotation/src/main/java/jrout/tutorial/springmvc/employee/service/EmployeeServiceImpl.java
+++ Spring/SpringMVCAnnotation/src/main/java/jrout/tutorial/springmvc/employee/service/EmployeeServiceImpl.java
@@ -1,14 +1,20 @@
 package jrout.tutorial.springmvc.employee.service;
 
+import jrout.tutorial.springmvc.employee.dao.EmployeeDao;
 import jrout.tutorial.springmvc.employee.model.Employee;
+
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeServiceImpl implements EmployeeService {
 
+	@Resource
+	private EmployeeDao employeeDao;
+	
 	@Override
 	public Employee getEmployeeDetail(int empId) {
-		return null;
+		return employeeDao.getEmployeeDetail(empId);
 	}
 }
