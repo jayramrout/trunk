27f967a77581254b6647abcae65a1b066a11d9c2
Jayram <jayram.rout@hotmail.com>
2015-12-09 21:39:25 -0500

    Update

--- WebServices/wstopdown/src/main/java/jayramrout/tutorial/ws/EmployeeEndpoint.java
+++ WebServices/wstopdown/src/main/java/jayramrout/tutorial/ws/EmployeeEndpoint.java
@@ -1,10 +1,12 @@
 package jayramrout.tutorial.ws;
 
-import jayramrout.tutorial.employee.schemas.EmployeeRequest;
-
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import jayramrout.tutorial.employee.schemas.EmployeeLeaveRequest;
+import jayramrout.tutorial.employee.schemas.EmployeeRequest;
 
 @Endpoint
 public class EmployeeEndpoint {
@@ -14,5 +16,10 @@ public class EmployeeEndpoint {
 	@PayloadRoot(namespace = "http://tutorial.jayramrout/employee/schemas", localPart = "EmployeeRequest")
 	public void printEmployeeDetails(@RequestPayload EmployeeRequest employeeRequest) {
 		System.out.println(employeeRequest.getEmployee().getFirstName());
+	}
+	
+	@PayloadRoot(namespace = "http://tutorial.jayramrout/employee/schemas", localPart = "EmployeeLeaveRequest")
+	public void printEmployeeDetails2(@RequestPayload EmployeeLeaveRequest employeeRequest) {
+		System.out.println("Leave Request "+ employeeRequest.getEmployee().getFirstName());
 	}
 }
