71e57163ca2cafea0b9bd689688c91ebf1f3cd1e
Jayram <jayram.rout@hotmail.com>
2015-12-17 21:56:17 -0500

    Update

--- RMIProject/src/rmidemoapp/RMIDemoServer.java
+++ RMIProject/src/rmidemoapp/RMIDemoServer.java
@@ -10,16 +10,17 @@ public class RMIDemoServer {
 		
 		/*
 		 * This need rmiregistry to be started prior to running this program
+		 * Run the program from command prompt
 		 */
-//		String url = "rmi://localhost/RMIDemo";
-//		Naming.rebind(url, impl);	
+		String url = "rmi://localhost/RMIDemo";
+		Naming.rebind(url, impl);	
 
 		/*
 		 * Below approach does not need rmiregistry to run prior to running this Program.
 		 * This takes care of binding and starting the server. 
 		 */
-		Registry registry = LocateRegistry.createRegistry(2345);
-		registry.rebind("RMIDemo", impl);
+		/*Registry registry = LocateRegistry.createRegistry(2345);
+		registry.rebind("RMIDemo", impl);*/
 		System.out.println("RMIDemoServer.main() Object Bound...");
 	}
 }
