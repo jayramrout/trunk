c45f53e66d6d0cb8e989469037701a0924f948dd
Jayram <jayram.rout@hotmail.com>
2015-12-17 21:44:08 -0500

    Update

--- RMIProject/src/rmidemoapp/RMIDemoClient.java
+++ RMIProject/src/rmidemoapp/RMIDemoClient.java
@@ -1,11 +1,17 @@
 package rmidemoapp;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RMIDemoClient {
 	public static void main(String[] args) throws Exception {
 		String url = "rmi://localhost/RMIDemo";
 		RMIDemo demo = (RMIDemo)Naming.lookup(url);
-		System.out.println(demo.doCommunicate("Jayram"));
+		System.out.println(demo.doCommunicate("Diyansha"));
+		
+		Registry registry = LocateRegistry.getRegistry("localhost",2345);
+		demo = (RMIDemo)registry.lookup("RMIDemo");
+		System.out.println(demo.doCommunicate("Diyansha"));
 	}
 }
--- RMIProject/src/rmidemoapp/RMIDemoServer.java
+++ RMIProject/src/rmidemoapp/RMIDemoServer.java
@@ -1,11 +1,25 @@
 package rmidemoapp;
 
 import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class RMIDemoServer {
 	public static void main(String[] args) throws Exception {
 		RMIDemoImpl impl = new RMIDemoImpl();
-		Naming.rebind("RMIDemo", impl);
+		
+		/*
+		 * This need rmiregistry to be started prior to running this program
+		 */
+		//Naming.rebind("RMIDemo", impl);	
+		
+		
+		/*
+		 * Below approach does not need rmiregistry to run prior to running this Program.
+		 * This takes care of binding and starting the server. 
+		 */
+		Registry registry = LocateRegistry.createRegistry(2345);
+		registry.rebind("RMIDemo", impl);
 		System.out.println("RMIDemoServer.main() Object Bound...");
 	}
 }
