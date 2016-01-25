fa02f6de085e967d9db058b72585023ab8fbea7b
Jayram <jayram.rout@hotmail.com>
2016-01-24 22:48:34 -0500

    Update

--- WebServices/wstopdown/src/main/java/jayramrout/tutorial/ws/EmployeeEndpoint.java
+++ WebServices/wstopdown/src/main/java/jayramrout/tutorial/ws/EmployeeEndpoint.java
@@ -6,7 +6,9 @@ import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import jayramrout.tutorial.employee.schemas.EmployeeLeaveRequest;
+import jayramrout.tutorial.employee.schemas.EmployeeMessage;
 import jayramrout.tutorial.employee.schemas.EmployeeRequest;
+import jayramrout.tutorial.employee.schemas.EmployeeType;
 
 @Endpoint
 public class EmployeeEndpoint {
@@ -18,8 +20,12 @@ public class EmployeeEndpoint {
 		System.out.println(employeeRequest.getEmployee().getFirstName());
 	}
 	
-	@PayloadRoot(namespace = "http://tutorial.jayramrout/employee/schemas", localPart = "EmployeeLeaveRequest")
-	public void printEmployeeDetails2(@RequestPayload EmployeeLeaveRequest employeeRequest) {
+	/*@PayloadRoot(namespace = "http://tutorial.jayramrout/employee/schemas", localPart = "EmployeeLeaveRequest")
+	@ResponsePayload
+	public EmployeeMessage getEmployee(@RequestPayload EmployeeLeaveRequest employeeRequest) {
 		System.out.println("Leave Request "+ employeeRequest.getEmployee().getFirstName());
-	}
+		EmployeeMessage employeeMessage = new EmployeeMessage();
+		employeeMessage.setMessage(employeeRequest.getEmployee().getFirstName() +" Leave Granted..");
+		return employeeMessage;
+	}*/
 }
