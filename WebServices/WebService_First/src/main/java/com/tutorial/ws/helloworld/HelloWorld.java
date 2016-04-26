bc4603daa5db0a8231d975ef5e6d5af346ee2472
Jayram <jayram.rout@hotmail.com>
2016-04-25 21:56:17 -0400

    update

--- WebServices/WebService_First/src/main/java/com/tutorial/ws/helloworld/HelloWorld.java
+++ WebServices/WebService_First/src/main/java/com/tutorial/ws/helloworld/HelloWorld.java
@@ -6,13 +6,15 @@ import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.ParameterStyle;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.jws.soap.SOAPBinding.Use;
+import javax.xml.bind.annotation.XmlElement;
 
 /**
- * @author Jayram
- * First Create a Web Service Endpoint Interface
+ * @author Jayram First Create a Web Service Endpoint Interface
+ * NOTE : @XmlElement annotation makes the argument compulsory.
  */
 @WebService
-@SOAPBinding(style = Style.DOCUMENT, parameterStyle = ParameterStyle.WRAPPED , use= Use.LITERAL)
+@SOAPBinding(style = Style.DOCUMENT, parameterStyle = ParameterStyle.WRAPPED, use = Use.LITERAL)
 public interface HelloWorld {
-	@WebMethod String getHelloWorld(String name, String nametwo);
+	@WebMethod
+	String getHelloWorld(@XmlElement(required = true) String firstName, String lastName);
 }
